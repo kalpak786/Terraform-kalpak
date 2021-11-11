4c16a58e40faa317b65ac345eb9a055a40252bda
kalpakkarmore <karmorekalpak5@gmail.com>
2021-11-11 19:37:18 +0530

    --- Auto-Git Commit ---

--- create-instance.tf
+++ create-instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "this" {
-  ami           = "ami-0f19d220602031aed"
+  ami           = "ami-0e0bf4b3a0c0e0adc"
   instance_type = "t2.micro"
   tags = {
     Name = "Web Server Test"
